cc64e6cb26fda29b3faa01ea74928d989029cb20
Kamiro <prietocamilo21@gmail.com>
2025-10-01 16:10:49 -0500

    NEW >>> Metodos CRUD
    Se agregaron metodos para JS con el proposito de que hagan peticiones CRUD a la base de datos.

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -11,4 +11,41 @@ router.post("/animalitos", (req, res) => {
         .catch((error) => res.json({ message: error }));
 });
 
+// Consultar los animales
+router.get("/animalitos", (req, res) => {
+    animalSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json ({ message: error }));
+});
+
+//Buscar animal por ID
+router.get("/animalitos/:id", (req, res) =>{
+    const {id} = req.params;
+    animalSchema
+        .findById(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({message: error}));
+});
+
+//Modificar animal por ID
+router.put("/animalitos/:id", (req, res) =>{
+    const {id} = req.params;
+    const {nombre, edad, tipo, fecha} = req.body;
+    animalSchema
+        .updateOne({ _id: id }, {
+            $set:{nombre, edad, tipo, fecha}
+        })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({message: error}));
+});
+
+//Eliminar un animal por ID
+router.delete("/animalitos/:id", (req, res) =>{
+    const {id} = req.params;
+    animalSchema
+        .findByIdAndDelete(id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({message: error}));
+});
+
 module.exports = router;
